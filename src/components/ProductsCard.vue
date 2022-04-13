c45d4376b971c3967027d3779d6d3e8e47952c8b
rafaelamicaela <rafaela.micaela@academico.ifrn.edu.br>
2022-04-13 17:35:30 -0300

    first commit

---
+++ src/components/ProductsCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="row justify-space-around">
+    <v-card class="" max-width="249">
+      <v-img src="../assets/Pasta.png" height="200px"></v-img>
+
+      <v-card-title> Pasta dental </v-card-title>
+
+      <v-card-subtitle
+        >Pasta dental menta e malaleuca sem Flúor - 90g</v-card-subtitle
+      >
+
+      <v-card-actions>
+        <v-btn
+          class="white--text"
+          depressed
+          elevation="2"
+          rounded
+          small
+          color="light-green darken-1"
+          >Comprar
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+    <v-card class="" max-width="249">
+      <v-img src="../assets/Pasta.png" height="200px"></v-img>
+
+      <v-card-title> Pasta dental </v-card-title>
+
+      <v-card-subtitle
+        >Pasta dental menta e malaleuca sem Flúor - 90g</v-card-subtitle
+      >
+
+      <v-card-actions>
+        <v-btn
+          class="white--text"
+          depressed
+          elevation="2"
+          rounded
+          small
+          color="light-green darken-1"
+          >Comprar
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+    <v-card class="" max-width="249">
+      <v-img src="../assets/Pasta.png" height="200px"></v-img>
+
+      <v-card-title> Pasta dental </v-card-title>
+
+      <v-card-subtitle
+        >Pasta dental menta e malaleuca sem Flúor - 90g</v-card-subtitle
+      >
+
+      <v-card-actions>
+        <v-btn
+          class="white--text"
+          depressed
+          elevation="2"
+          rounded
+          small
+          color="light-green darken-1"
+          >Comprar
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+    <v-card class="" max-width="249">
+      <v-img src="../assets/Pasta.png" height="200px"></v-img>
+
+      <v-card-title> Pasta dental </v-card-title>
+
+      <v-card-subtitle
+        >Pasta dental menta e malaleuca sem Flúor - 90g</v-card-subtitle
+      >
+
+      <v-card-actions>
+        <v-btn
+          class="white--text"
+          depressed
+          elevation="2"
+          rounded
+          small
+          color="light-green darken-1"
+          >Comprar
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
+<style>
+</style>
